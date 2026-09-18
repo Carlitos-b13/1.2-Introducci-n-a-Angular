f11cdf6f00c21f95312714b3e0e9054d1d5c1e1e
Carlos Benitez <carlos.benitezarciniega@gmail.com>
2026-09-18 09:43:57 -0700

    Practica inicial Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,54 @@
 import { Component, signal } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-root',
-  styleUrl: './app.css',
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('practica-talleres');
+  titulo = 'Talleres disponibles';
+
+  talleres = [
+    {
+      id: 1,
+      nombre: 'Introducción a HTML',
+      duracion: '2 horas'
+    },
+    {
+      id: 2,
+      nombre: 'CSS básico',
+      duracion: '3 horas'
+    },
+    {
+      id: 3,
+      nombre: 'JavaScript',
+      duracion: '4 horas'
+    },
+    {
+      id: 4,
+      nombre: 'Angular',
+      duracion: '4 horas'
+    },
+    {
+      id: 5,
+      nombre: 'React',
+      duracion: '5 horas'
+    }
+  ];
+
+  inscritos = signal<number[]>([]);
+
+  inscribirse(id: number) {
+    if (this.inscritos().includes(id)) {
+      return;
+    }
+
+    this.inscritos.update(actuales => [...actuales, id]);
+  }
+
+  cancelarInscripcion(id: number) {
+    this.inscritos.update(actuales =>
+      actuales.filter(tallerId => tallerId !== id)
+    );
+  }
 }
